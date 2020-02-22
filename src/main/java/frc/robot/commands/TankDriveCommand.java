31cdefecf77630bcf233067442d33cb9ff603958
GGSammy <vineoxen@gmail.com>
2020-02-22 13:19:32 -0500

    S6.2.0B - Added drive stick deadband
    Implemented a scaling deadband to the TankDriveCommand.
    
    Co-Authored-By: bnjimenez <bnjimenez@users.noreply.github.com>

--- src/main/java/frc/robot/commands/TankDriveCommand.java
+++ src/main/java/frc/robot/commands/TankDriveCommand.java
@@ -35,19 +35,25 @@ public class TankDriveCommand extends CommandBase {
   // Called repeatedly when this Command is scheduled to run
   @Override
   public void execute() {
+    double speed = 1;
+
+    /*
+    Rough deadband
+    
     double left = -Devices.leftStick.getY();
     double right = -Devices.rightStick.getY();
-
     if (Math.abs(left) < 0.1) {
         left = 0;
     }
     if (Math.abs(right) < 0.1) {
         right = 0;
     }
-    
-    double speed = 1;
-    
     Subsystems.Drive.set(left * speed, right * speed);
+    */
+
+    double leftValue = applyDeadband(-Devices.leftStick.getY(), 0.1);
+    double rightValue = applyDeadband(-Devices.rightStick.getY(), 0.1);
+    Subsystems.Drive.set(leftValue * speed, rightValue * speed);
   }
 
   // Make this return true when this Command no longer needs to run execute()
@@ -60,5 +66,17 @@ public class TankDriveCommand extends CommandBase {
   @Override
   public void end(boolean interrupted) {
     
+  }
+
+  private double applyDeadband(double value, double deadband) {
+    if (Math.abs(value) > deadband) {
+      if (value > 0.0) {
+        return (value - deadband) / (1.0 - deadband);
+      } else {
+        return (value + deadband) / (1.0 - deadband);
+      }
+    } else {
+      return 0.0;
+    }
   }
 }
